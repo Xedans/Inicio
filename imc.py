baab5b3aaa7b21c279c79fde5f405233f4e8a835
Xedans <142552631+Xedans@users.noreply.github.com>
2023-10-03 13:15:06 -0300

    imc.py

--- imc.py
+++ imc.py
@@ -1,13 +1,15 @@
+#Começamos definindo uma função
 def calcular_imc(peso, altura):
+    #Aqui colocamos a formula do IMC que é: peso / (altura * altura)
     imc = peso / (altura ** 2)
     return imc
-
+#Solicitamos os dados do paciente em questão
 peso = float(input('Digite seu peso em KG: '))
 altura = float(input('Digite sua altura em metros: '))
 imc = calcular_imc(peso, altura)
 
 print('Seu IMC é {:.2f}'.format(imc))
-
+#E por ultimo damos um diagnóstico de acordo com os cálculos feitos.
 if imc < 18.5:
     print('Você está abaixo do peso.')
 elif 18.5 <= imc < 24.9:
